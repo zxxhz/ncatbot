9b7e32ea2ca985fc98b0bd4b7b9147ad28519860
liyihao1110 <2793415370@qq.com>
2024-11-28 18:00:38 +0800

    refactor(client): 移除不必要的 at 消息类型
    -从 client.py 中删除了 at消息类型的定义
    - 在 message.py 中添加了 AtMessage 类来处理 at 消息
    - 修改了 BaseMessage 类中的 __get_messages_by_type 方法，使其返回单个消息的数据
    - 更新了所有消息类型的 property 方法，使其返回对应的 Message 类实例

--- Ncatbot/message.py
+++ Ncatbot/message.py
@@ -1,6 +1,156 @@
 from .api import BotAPI
 from .settings import settings
 
+class TextMessage:
+    def __init__(self, data):
+        self.text = data.get('text', '')
+
+    def __str__(self):
+        return str(self.__dict__)
+
+class FaceMessage:
+    def __init__(self, data):
+        self.id = data.get('id', None)
+
+    def __str__(self):
+        return str(self.__dict__)
+
+class ImageMessage:
+    def __init__(self, data):
+        self.name = data.get('name', None)
+        self.summary = data.get('summary', None)
+        self.file = data.get('file', None)
+        self.sub_type = data.get('sub_type', None)
+        self.file_id = data.get('file_id', None)
+        self.url = data.get('url', None)
+        self.path = data.get('path', None)
+        self.file_size = data.get('file_size', None)
+        self.file_unique = data.get('file_unique', None)
+
+    def __str__(self):
+        return str(self.__dict__)
+
+class RecordMessage:
+    def __init__(self, data):
+        self.file = data.get('file', None)
+        self.name = data.get('name', None)
+        self.url = data.get('url', None)
+        self.path = data.get('path', None)
+        self.file_id = data.get('file_id', None)
+        self.file_size = data.get('file_size', None)
+        self.file_unique = data.get('file_unique', None)
+
+    def __str__(self):
+        return str(self.__dict__)
+
+class VideoMessage:
+    def __init__(self, data):
+        self.name = data.get('name', None)
+        self.file = data.get('file', None)
+        self.thumb = data.get('thumb', None)
+        self.url = data.get('url', None)
+        self.path = data.get('path', None)
+        self.file_id = data.get('file_id', None)
+        self.file_size = data.get('file_size', None)
+        self.file_unique = data.get('file_unique', None)
+
+    def __str__(self):
+        return str(self.__dict__)
+
+class AtMessage:
+    def __init__(self, data):
+        self.qq = data.get('qq', None)
+
+    def __str__(self):
+        return str(self.__dict__)
+
+class RpsMessage:
+    def __init__(self, data):
+        self.result = data.get('result', None)
+
+    def __str__(self):
+        return str(self.__dict__)
+
+class DiceMessage:
+    def __init__(self, data):
+        self.result = data.get('result', None)
+
+    def __str__(self):
+        return str(self.__dict__)
+
+class ContactMessage:
+    def __init__(self, data):
+        self.type = data.get('type', None)
+        self.id = data.get('id', None)
+
+    def __str__(self):
+        return str(self.__dict__)
+
+class MusicMessage:
+    def __init__(self, data):
+        self.type = data.get('type', None)
+        self.id = data.get('id', None)
+        self.url = data.get('url', None)
+        self.audio = data.get('audio', None)
+        self.title = data.get('title', None)
+        self.image = data.get('image', None)
+        self.singer = data.get('singer', None)
+
+    def __str__(self):
+        return str(self.__dict__)
+
+class ReplyMessage:
+    def __init__(self, data):
+        self.id = data.get('id', None)
+
+    def __str__(self):
+        return str(self.__dict__)
+
+class ForwardMessage:
+    def __init__(self, data):
+        self.id = data.get('id', None)
+        self.content = data.get('content', [])
+
+    def __str__(self):
+        return str(self.__dict__)
+
+class NodeMessage:
+    def __init__(self, data):
+        self.user_id = data.get('user_id', None)
+        self.nickname = data.get('nickname', None)
+        self.id = data.get('id', None)
+        self.content = data.get('content', [])
+
+    def __str__(self):
+        return str(self.__dict__)
+
+class JsonMessage:
+    def __init__(self, data):
+        self.data = data.get('data', None)
+
+    def __str__(self):
+        return str(self.__dict__)
+
+class MfaceMessage:
+    def __init__(self, data):
+        self.emoji_id = data.get('emoji_id', None)
+        self.emoji_package_id = data.get('emoji_package_id', None)
+        self.key = data.get('key', None)
+        self.summary = data.get('summary', None)
+
+    def __str__(self):
+        return str(self.__dict__)
+
+class FileMessage:
+    def __init__(self, data):
+        self.name = data.get('name', None)
+        self.file = data.get('file', None)
+        self.path = data.get('path', None)
+        self.url = data.get('url', None)
+        self.file_id = data.get('file_id', None)
+        self.file_size = data.get('file_size', None)
+        self.file_unique = data.get('file_unique', None)
+
 
 class BaseMessage(BotAPI):
     def __init__(self, message):
@@ -45,39 +195,68 @@ class GroupMessage(BaseMessage):
             return str(self.message)
 
         def __get_messages_by_type(self, msg_type):
-            return [msg['data'][list(msg['data'].keys())[0]] for msg in self.message if msg['type'] == msg_type]
+            for msg in self.message:
+                if msg['type'] == msg_type:
+                    return msg.get('data',{})
 
         @property
         def text(self):
-            return self.__get_messages_by_type('text')
+            text_data = self.__get_messages_by_type('text')
+            if text_data:
+                return TextMessage(text_data)
+            return None
+
+
 
         @property
         def face(self):
-            return self.__get_messages_by_type('face')
+            face_data = self.__get_messages_by_type('face')
+            if face_data:
+                return FaceMessage(face_data)
+            return None
 
         @property
         def image(self):
-            return self.__get_messages_by_type('image')
+            image_data = self.__get_messages_by_type('image')
+            if image_data:
+                return ImageMessage(image_data)
+            return None
 
         @property
         def record(self):
-            return self.__get_messages_by_type('record')
+            record_data = self.__get_messages_by_type('record')
+            if record_data:
+                return RecordMessage(record_data)
+            return None
 
         @property
         def video(self):
-            return self.__get_messages_by_type('video')
-
-        @property
-        def at(self):
-            return self.__get_messages_by_type('at')
+            video_data = self.__get_messages_by_type('video')
+            if video_data:
+                return VideoMessage(video_data)
+            return None
 
         @property
         def rps(self):
-            return self.__get_messages_by_type('rps')
+            rps_data = self.__get_messages_by_type('rps')
+            if rps_data:
+                return RpsMessage(rps_data)
+            return None
 
         @property
         def dice(self):
-            return self.__get_messages_by_type('dice')
+            dice_data = self.__get_messages_by_type('dice')
+            if dice_data:
+                return DiceMessage(dice_data)
+            return None
+
+        @property
+        def at(self):
+            at_data = self.__get_messages_by_type('at')
+            if at_data:
+                return AtMessage(at_data)
+            return None
+
 
         @property
         def poke(self):
@@ -97,31 +276,52 @@ class GroupMessage(BaseMessage):
 
         @property
         def music(self):
-            return self.__get_messages_by_type('music')
+            music_data = self.__get_messages_by_type('music')
+            if music_data:
+                return MusicMessage(music_data)
+            return None
 
         @property
         def reply(self):
-            return self.__get_messages_by_type('reply')
+            reply_data = self.__get_messages_by_type('reply')
+            if reply_data:
+                return ReplyMessage(reply_data)
+            return None
 
         @property
         def forward(self):
-            return self.__get_messages_by_type('forward')
+            forward_data = self.__get_messages_by_type('forward')
+            if forward_data:
+                return ForwardMessage(forward_data)
+            return None
 
         @property
         def node(self):
-            return self.__get_messages_by_type('node')
+            node_data = self.__get_messages_by_type('node')
+            if node_data:
+                return NodeMessage(node_data)
+            return None
 
         @property
         def json(self):
-            return self.__get_messages_by_type('json')
+            json_data = self.__get_messages_by_type('json')
+            if json_data:
+                return JsonMessage(json_data)
+            return None
 
         @property
         def mface(self):
-            return self.__get_messages_by_type('mface')
+            mface_data = self.__get_messages_by_type('mface')
+            if mface_data:
+                return MfaceMessage(mface_data)
+            return None
 
         @property
         def file(self):
-            return self.__get_messages_by_type('file')
+            file_data = self.__get_messages_by_type('file')
+            if file_data:
+                return FileMessage(file_data)
+            return None
 
         @property
         def lightapp(self):
@@ -169,39 +369,60 @@ class PrivateMessage(BaseMessage):
 
 
         def __get_messages_by_type(self, msg_type):
-            return [msg['data'][list(msg['data'].keys())[0]] for msg in self.message if msg['type'] == msg_type]
+            for msg in self.message:
+                if msg['type'] == msg_type:
+                    return msg.get('data',{})
 
         @property
         def text(self):
-            return self.__get_messages_by_type('text')
+            text_data = self.__get_messages_by_type('text')
+            if text_data:
+                return TextMessage(text_data)
+            return None
+
+
 
         @property
         def face(self):
-            return self.__get_messages_by_type('face')
+            face_data = self.__get_messages_by_type('face')
+            if face_data:
+                return FaceMessage(face_data)
+            return None
 
         @property
         def image(self):
-            return self.__get_messages_by_type('image')
+            image_data = self.__get_messages_by_type('image')
+            if image_data:
+                return ImageMessage(image_data)
+            return None
 
         @property
         def record(self):
-            return self.__get_messages_by_type('record')
+            record_data = self.__get_messages_by_type('record')
+            if record_data:
+                return RecordMessage(record_data)
+            return None
 
         @property
         def video(self):
-            return self.__get_messages_by_type('video')
-
-        @property
-        def at(self):
-            return self.__get_messages_by_type('at')
+            video_data = self.__get_messages_by_type('video')
+            if video_data:
+                return VideoMessage(video_data)
+            return None
 
         @property
         def rps(self):
-            return self.__get_messages_by_type('rps')
+            rps_data = self.__get_messages_by_type('rps')
+            if rps_data:
+                return RpsMessage(rps_data)
+            return None
 
         @property
         def dice(self):
-            return self.__get_messages_by_type('dice')
+            dice_data = self.__get_messages_by_type('dice')
+            if dice_data:
+                return DiceMessage(dice_data)
+            return None
 
         @property
         def poke(self):
@@ -221,31 +442,52 @@ class PrivateMessage(BaseMessage):
 
         @property
         def music(self):
-            return self.__get_messages_by_type('music')
+            music_data = self.__get_messages_by_type('music')
+            if music_data:
+                return MusicMessage(music_data)
+            return None
 
         @property
         def reply(self):
-            return self.__get_messages_by_type('reply')
+            reply_data = self.__get_messages_by_type('reply')
+            if reply_data:
+                return ReplyMessage(reply_data)
+            return None
 
         @property
         def forward(self):
-            return self.__get_messages_by_type('forward')
+            forward_data = self.__get_messages_by_type('forward')
+            if forward_data:
+                return ForwardMessage(forward_data)
+            return None
 
         @property
         def node(self):
-            return self.__get_messages_by_type('node')
+            node_data = self.__get_messages_by_type('node')
+            if node_data:
+                return NodeMessage(node_data)
+            return None
 
         @property
         def json(self):
-            return self.__get_messages_by_type('json')
+            json_data = self.__get_messages_by_type('json')
+            if json_data:
+                return JsonMessage(json_data)
+            return None
 
         @property
         def mface(self):
-            return self.__get_messages_by_type('mface')
+            mface_data = self.__get_messages_by_type('mface')
+            if mface_data:
+                return MfaceMessage(mface_data)
+            return None
 
         @property
         def file(self):
-            return self.__get_messages_by_type('file')
+            file_data = self.__get_messages_by_type('file')
+            if file_data:
+                return FileMessage(file_data)
+            return None
 
         @property
         def lightapp(self):
