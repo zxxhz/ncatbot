e8f19fcbaf33d544b73716eace57c76bd6a10152
liyihao1110 <2793415370@qq.com>
2025-01-24 10:12:03 +0800

    refactor(main): 重构主程序以使用 ncatbot 框架
    - 替换 ncatpy 为 ncatbot框架
    - 重新设计事件处理逻辑，使用装饰器注册事件处理函数
    - 添加对通知事件和请求事件的处理
    -简化程序结构，提高可读性和可维护性

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-# encoding: utf-8
 
-import ncatpy
-from ncatpy import logging
-from ncatpy.message import GroupMessage,PrivateMessage
+from ncatbot.client import BotClient
+from ncatbot.message import GroupMessage,PrivateMessage
 
-_log = logging.get_logger()
+bot = BotClient()
 
-class MyClient(ncatpy.Client):
-    async def on_group_message(self, message: GroupMessage):
-        _log.info(message)
+@bot.group_event
+async def on_group_message(msg:GroupMessage):
+    print(msg)
 
-    async def on_private_message(self, message: PrivateMessage):
-        _log.info(message)
+@bot.private_event
+async def on_private_message(msg:PrivateMessage):
+    print(msg)
 
-if __name__ == "__main__":
-    # 1. 通过预设置的类型，设置需要监听的事件通道
-    # intents = ncatpy.Intents.public() # 可以订阅public，包括了私聊和群聊
+@bot.notice_event
+async def on_notice(msg):
+    print(msg)
 
-    # 2. 通过kwargs，设置需要监听的事件通道
-    intents = ncatpy.Intents(group_event=True)
-    client = MyClient(intents=intents)
-    client.run()
+@bot.request_event
+async def on_request(msg):
+    print(msg)
+
+bot.run(reload=True)
