3bd59a317d760946d749717206b8c22024b164b0
liyihao1110 <2793415370@qq.com>
2025-01-24 10:09:36 +0800

    feat(bot): 添加 AI 机器人功能
    - 实现了基本的 AI 对话功能，支持群聊和私聊
    - 添加了历史记录管理功能，可以清除历史记录- 使用 OpenAI API 进行对话生成

---
+++ ncatbot/bot.py
@@ -0,0 +1,68 @@
+
+from .api import BotAPI
+from .setting import SetConfig
+
+from openai import OpenAI
+
+_set = SetConfig()
+
+class Bot:
+    def __init__(self):
+        self.__api = BotAPI(use_ws=True)
+        self.__url = _set.base_url
+        self.__key = _set.api_key
+        self.__model = _set.ai_model
+        self.__personality = _set.personality
+        self.__messages = [{"role": "system", "content": self.__personality}]
+
+    async def ai_response(self, input, group_id=None, user_id=None):
+        client = OpenAI(
+            api_key=self.__key,
+            base_url=self.__url,
+        )
+        completion = client.chat.completions.create(
+            model='generalv3.5',
+            messages=[
+                {
+                    "role": "system",
+                    "content": self.__personality
+                },
+                {
+                    "role": "user",
+                    "content": input
+                }
+            ]
+        )
+        if group_id:
+            await self.__api.post_group_msg(group_id, completion.choices[0].message.content)
+        else:
+            await self.__api.post_private_msg(user_id, completion.choices[0].message.content)
+
+    async def ai_response_history(self, input, history_num = 5, group_id=None, user_id=None):
+        if len(self.__messages)/2 > history_num:
+            self.__messages.pop(1)
+        elif input == ".clear" and group_id:
+            self.__messages = [{"role": "system", "content": self.__personality}]
+            await self.__api.post_group_msg(group_id, "已清空历史记录")
+            return
+        elif input == ".clear" and user_id:
+            self.__messages = [{"role": "system", "content": self.__personality}]
+            await self.__api.post_private_msg(user_id, "已清空历史记录")
+            return
+        self.__messages.append({"role": "user", "content": input})
+        client = OpenAI(
+            api_key=self.__key,
+            base_url=self.__url,
+        )
+        completion = client.chat.completions.create(
+            model='generalv3.5',
+            messages=self.__messages
+        )
+        if group_id:
+            await self.__api.post_group_msg(group_id, completion.choices[0].message.content)
+            self.__messages.append({"role": "assistant", "content": completion.choices[0].message.content})
+        elif user_id:
+            await self.__api.post_private_msg(user_id, completion.choices[0].message.content)
+            self.__messages.append({"role": "assistant", "content": completion.choices[0].message.content})
+
+
