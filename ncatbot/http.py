6dda85447685b4c445fa3ab8ff8588c5808a496a
木子 <113816301+liyihao1110@users.noreply.github.com>
2025-02-03 09:53:13 +0800

    refactor(http): 重构 HTTP 和 WebSocket 连接逻辑
    - 更新了 HTTP 和 WebSocket 路由类的初始化逻辑
    -调整了配置项的引用，使用更简洁的命名
    -优化了 WebSocket 连接过程，添加了额外的头部信息
    - 移除了冗余的空行和不必要的代码

--- ncatbot/http.py
+++ ncatbot/http.py
@@ -1,17 +1,16 @@
-
 import httpx
 import datetime
 import json as j
 import websockets
 
-from .setting import SetConfig
+from .config import SetConfig
 
 _set = SetConfig()
 
 class Route:
     def __init__(self):
-        self.headers = {'Content-Type': 'application/json', 'Authorization': f'Bearer {_set.http_token}'} if _set.http_token else {'Content-Type': 'application/json'}
-        self.url = _set.http_url
+        self.headers = {'Content-Type': 'application/json', 'Authorization': f'Bearer {_set.token}'} if _set.token else {'Content-Type': 'application/json'}
+        self.url = _set.hp_uri
 
     async def get(self, path, params=None):
         async with httpx.AsyncClient() as client:
@@ -28,15 +27,14 @@ class Route:
 
 class WsRoute:
     def __init__(self):
-        self.url = _set.ws_url+'/api'
-        self.headers = {'Content-Type': 'application/json'}
+        self.url = _set.ws_uri+'/api'
+        self.headers = {'Content-Type': 'application/json', 'Authorization': f'Bearer {_set.token}'} if _set.token else {'Content-Type': 'application/json'}
 
     async def post(self, path, params=None, json=None):
-        async with websockets.connect(self.url) as websocket:
+        async with websockets.connect(self.url, extra_headers=self.headers) as websocket:
             if params:
                 await websocket.send(j.dumps({'action': path.replace('/', ''), 'params': params, 'echo': int(datetime.datetime.now().timestamp())}))
             elif json:
                 await websocket.send(j.dumps({'action': path.replace('/', ''), 'params': json, 'echo': int(datetime.datetime.now().timestamp())}))
             response = await websocket.recv()
             return j.loads(response)
-
