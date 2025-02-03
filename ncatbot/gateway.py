515eb0c581e21a004aa17531898eaf1658cd5990
木子 <113816301+liyihao1110@users.noreply.github.com>
2025-02-03 09:52:50 +0800

    refactor(client): 重构客户端模块并优化日志记录
    -重命名模块和变量以提高代码可读性
    - 引入日志模块以替换 print 语句
    - 更新配置项名称以更好地反映其用途
    - 删除冗余代码并优化程序结构

--- ncatbot/gateway.py
+++ ncatbot/gateway.py
@@ -1,13 +1,15 @@
 import websockets
 import json
 
-from .setting import SetConfig
-from .user import User
+from .config import SetConfig
+from .logger import get_log
+
+_log = get_log()
+_set = SetConfig()
 
 class Websocket:
     def __init__(self, client):
         self.client = client
-        self.user = User()
 
     async def receive(self, message):
         msg = json.loads(message)
@@ -17,46 +19,28 @@ class Websocket:
             elif msg['message_type'] == 'private':
                 return await self.client.handle_private_event(msg)
             else:
-                print("[gateway] 这个报错说明message_type不属于group,private\n"+str(msg))
+                _log.error("这个报错说明message_type不属于group,private\n"+str(msg))
         elif msg['post_type'] == 'notice':
             return await self.client.handle_notice_event(msg)
         elif msg['post_type'] == 'request':
             return await self.client.handle_request_event(msg)
         elif msg['post_type'] == 'meta_event':
             if msg['meta_event_type'] == 'lifecycle':
-                print(f"[gateway] 机器人 {msg.get('self_id')} 成功启动")
+                _log.info(f"机器人 {msg.get('self_id')} 成功启动")
             else:
                 pass
         else:
-            print(f"[gateway] 如果没错，这是一个错误，请反馈给开发者\n"+str(msg))
+            _log.error("这是一个错误，请反馈给开发者\n"+str(msg))
 
     async def ws_connect(self):
         try:
-            async with websockets.connect(uri=SetConfig().ws_url,
-                                          extra_headers={"Authorization": SetConfig().ws_token}) as ws:
+            async with websockets.connect(uri=_set.ws_uri+"/event", extra_headers={'Content-Type': 'application/json', 'Authorization': f'Bearer {_set.token}'} if _set.token else {'Content-Type': 'application/json'}) as ws:
                 try:
-                    print("[gateway] websocket连接已建立")
+                    _log.info("websocket连接已建立")
                     while True:
                         message = await ws.recv()
                         await self.receive(message)
-                        await self.user.on_private_msg(message)
                 except Exception as e:
                     raise e
         except Exception as e:
             raise e
-        finally:
-            print("[gateway] 切换连接方式...这是一个bug，如果你看见了这条输出内容，请不要添加token并提交issue")
-            async with websockets.connect(uri=SetConfig().ws_url) as ws:
-                try:
-                    print("[gateway] websocket连接已建立")
-                    while True:
-                        message = await ws.recv()
-                        await self.receive(message)
-                        await self.user.on_private_msg(message)
-                except Exception as e:
-                    raise e
-
-
-
-
-
