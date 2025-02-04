285cc032e8198e5422fe9e1b168f53ebaf03ac7a
木子 <113816301+liyihao1110@users.noreply.github.com>
2025-02-04 15:51:53 +0800

    feat(main): 添加处理 "md" 命令的功能
    - 当用户发送 "md" 时，通过私信发送 "README.md" 文件
    - 暂时注释掉了发送 markdown 内容的代码

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ async def on_private_message(msg:PrivateMessage):
         await bot.api.post_private_msg(msg.user_id, text="你好,",face=1, reply=msg.message_id)
     elif msg.raw_message == "ping":
         await bot.api.add_at(msg.user_id).add_face(1).add_text("你好").add_face(2).send_private_msg(msg.user_id,reply=msg.message_id)
+    elif msg.raw_message == "md":
+        await bot.api.post_private_file(user_id=msg.user_id,markdown="README.md")
+        # await bot.api.post_private_msg(msg.user_id, markdown="## 你好")
 
 @bot.notice_event
 async def on_notice(msg):
