eda824d1937bbbe2d63eccf61bdd301cddd1643e
Fish-LP <fish.zh@outlook.com>
2025-02-10 16:32:41 +0800

    日志设置细化 插件系统修改 默认设置修改

--- ncatbot/logger.py
+++ ncatbot/logger.py
@@ -8,7 +8,7 @@ from tqdm import tqdm as tqdm_original
 
 class Color:
     """
-    用于在终端中显示彩和样式。
+    用于在终端中显示颜色和样式。
 
     包含以下功能：
     - 前景：设置颜
@@ -35,7 +35,7 @@ class Color:
     WHITE = "\033[37m"
     """前景-白"""
     RESET = "\033[0m"
-    """重置所有颜和样式"""
+    """重置所有颜色和样式"""
 
     # 扩展前景
     LIGHT_GRAY = "\033[90m"
@@ -156,7 +156,8 @@ class ColoredFormatter(logging.Formatter):
 # 初始化日志配置
 def setup_logging():
     # 从环境变量中读取日志配置，如果环境变量不存在则使用默认值
-    log_level = os.getenv("LOG_LEVEL", "INFO").upper()  # 确保日志级别为大写
+    console_log_level = os.getenv("LOG_LEVEL", "INFO").upper()
+    file_log_level = os.getenv("FILE_LOG_LEVEL", "DEBUG").upper()
     log_format = os.getenv(
         "LOG_FORMAT",
         "[%(levelname)s] (%(filename)s:%(lineno)d) %(funcName)s : %(message)s",
@@ -169,12 +170,6 @@ def setup_logging():
     log_file_name = os.getenv("LOG_FILE_NAME", "bot_%Y%m%d.log")
     backup_count = int(os.getenv("BACKUP_COUNT", 7))
 
-    # export LOG_LEVEL=INFO
-    # export LOG_FORMAT='[%(asctime)s] %(levelname)s: %(message)s'
-    # export LOG_FILE_PATH=./logs
-    # export LOG_FILE_NAME=Ncatbot_%Y%m%d.log
-    # export BACKUP_COUNT=7
-
     # 创建日志文件夹
     os.makedirs(log_file_path, exist_ok=True)
 
@@ -185,13 +180,14 @@ def setup_logging():
 
     # 创建日志对象
     logger = logging.getLogger()
-    logger.setLevel(log_level)
+    logger.setLevel(console_log_level)  # 设置全局最低日志等级
 
-    # 创建控制台处理器并设置格式化器
+    # 创建控制台处理器并设置格式化器和日志等级
     console_handler = logging.StreamHandler()
+    console_handler.setLevel(console_log_level)  # 设置控制台日志等级
     console_handler.setFormatter(ColoredFormatter(log_format))
 
-    # 创建文件处理器并设置格式化器
+    # 创建文件处理器并设置格式化器和日志等级
     file_handler = TimedRotatingFileHandler(
         filename=log_file_full_path,
         when="midnight",
@@ -199,6 +195,7 @@ def setup_logging():
         backupCount=backup_count,
         encoding="utf-8",
     )
+    file_handler.setLevel(file_log_level)  # 设置文件日志等级
     file_handler.setFormatter(logging.Formatter(log_file_format))
 
     # 将处理器添加到日志对象
@@ -213,3 +210,27 @@ setup_logging()
 def get_log(name=None):
     """兼容方法"""
     return logging.getLogger("ncatbot" if name is None else name)
+
+# 示例用法
+if __name__ == "__main__":
+    from time import sleep
+    from tqdm.contrib.logging import logging_redirect_tqdm
+    logger = get_log(__name__)
+    logger.debug("这是一个调试信息")
+    logger.info("这是一个普通信息")
+    logger.warning("这是一个警告信息")
+    logger.error("这是一个错误信息")
+    logger.critical("这是一个严重错误信息")
+    # 常见参数
+    # total：总进度数。
+    # desc：进度条描述。
+    # ncols：进度条宽度。
+    # unit：进度单位。
+    # leave：是否在完成后保留进度条。
+
+    with logging_redirect_tqdm():
+        with tqdm(range(0,100)) as pbar:
+            for i in pbar:
+                if i % 10 == 0:
+                    logger.info(f'now: {i}')
+                sleep(0.1)
